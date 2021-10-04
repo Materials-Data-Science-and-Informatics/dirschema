c3eed5d25e692f78971d43874835378209ce7996
a.pirogov <a.pirogov@fz-juelich.de>
2021-10-04 17:13:36 +0200

    feat: improve error messages closing #2

--- dirschema/validator.py
+++ dirschema/validator.py
@@ -34,7 +34,7 @@ class DSEvalCtx:
     matchStop: int = 0
     matchPat: Optional[re.Pattern] = None
 
-    debug: bool = False
+    # debug: bool = True
 
     def updated_context(self, rule: Rule) -> DSEvalCtx:
         """Return a new context updated with fields from the given rule."""
@@ -81,6 +81,12 @@ class DSValidator:
         """
         Validate a directory and return all validation errors (unsatisfied rules).
 
+        This function will pick the correct interface for interpreting "files" and
+        "directories", depending on whether the provided file is a directory or a
+        supported kind of archive file.
+        Depending on the used metadata convention, the companion metadata files will be
+        filtered out from the set of validated paths.
+
         In case of success the returned dict is empty.
         Otherwise the dict contains for each path with validation errors
         a residual rule that was not satisfied.
@@ -90,10 +96,8 @@ class DSValidator:
         ctx = DSEvalCtx(dirAdapter=dir, metaConvention=self.metaConvention, **kwargs)
         errors = {}
         for p in paths:
-            # print("schema before:", self.schema)
             res = self.validate_path(p, self.schema, ctx)
-            # print("schema after:", self.schema)
-            if res:  # or ("all_results" in kwargs and kwargs["all_results"]):
+            if res:
                 errors[p] = res
         return errors
 
@@ -104,6 +108,8 @@ class DSValidator:
         If a stream is provided, prints it out. Otherwise, returns string.
         """
         of = stream or io.StringIO()
+        # this round-trip is ugly, but the simplest way to get nice YAML output.
+        # need to use pydantic's .json() to eliminate non-standard types before YAMLizing
         tmp = {k: json.loads(v.json(exclude_defaults=True)) for k, v in errs.items()}
         yaml.dump(tmp, of)
         if not stream:
@@ -113,18 +119,22 @@ class DSValidator:
     def validate_path(
         self, path: str, rule: DSRule, ctx: DSEvalCtx
     ) -> Optional[DSRule]:
-        """Apply rule to path, return rule with only the unsatisfied constraints."""
-        if isinstance(rule.__root__, bool):
+        """
+        Apply rule to path under given evaluation context.
+
+        Return None on success or a partial rule indicating violated constraints.
+        """
+        if isinstance(rule.__root__, bool):  # trivial rule
             return None if rule.__root__ else rule.copy()
 
-        if ctx.debug:
-            print("validate_rule", f"for '{path}' rule:", repr(rule.__root__))
+        # if ctx.debug:
+        #     print("validate_rule", f"for '{path}' rule:", repr(rule.__root__))
 
         rl: Rule = cast(Rule, rule.__root__)  # used to check constraints
         curCtx = ctx.updated_context(rl)  # derived from parent ctx + current settings
         err = Rule()  # used to collect errors
 
-        # match / rewrite
+        # 1. match / rewrite
         # if rewrite is set, don't need do do separate match and just try rewriting
         psl = PathSlice.into(path, curCtx.matchStart, curCtx.matchStop)
         thenPath: str = path  # to be used for implication later on
@@ -132,14 +142,11 @@ class DSValidator:
             rewritten = psl.rewrite(curCtx.matchPat, rl.rewrite)
             if rewritten:
                 thenPath = rewritten.unslice()
-                # print(f"rewritten {path} -> {thenPath}")
-            else:
-                if rl.match:
-                    err.match = rl.match
-                if rl.rewrite:
-                    err.rewrite = rl.rewrite
+            else:  # failed match or rewrite
+                err.match = curCtx.matchPat
+                return DSRule(__root__=err)
 
-        # now proceed with the other primitive constraints
+        # 2. proceed with the other primitive constraints
 
         # take care of type constraint
         is_file = curCtx.dirAdapter.is_file(path)
@@ -160,7 +167,7 @@ class DSValidator:
             if schema is not None:
                 metapath = path
                 if key == "validMeta":
-                    metapath = curCtx.metaConvention.meta_for(path)
+                    metapath = curCtx.metaConvention.meta_for(path, is_dir=is_dir)
 
                 dat = ctx.dirAdapter.load_json(metapath)
                 validationError = False
@@ -176,43 +183,49 @@ class DSValidator:
                     if key == "validMeta":
                         err._metaPath = metapath
 
-        # next check the complex constraints
+        if err:
+            return DSRule(__root__=err)
+
+        # 3. check the complex constraints
         for op in ("allOf", "anyOf", "oneOf"):
             val = rl.__dict__[op]
             num_rules = len(val)
-
             if num_rules == 0:
-                continue
+                continue  # empty list of rules -> nothing to do
 
             num_fails = 0
             suberrs = []
             for r in val:
                 e = self.validate_path(path, r, curCtx)
-                if e:
-                    suberrs.append(e)
+                suberrs.append(e if e is not None else DSRule(True))
+                if e is not None:
                     num_fails += 1
                 elif op == "anyOf":
                     break  # we have a satisfied rule
 
             if op == "allOf" and num_fails > 0:
-                err.__dict__[op] = val
+                err.__dict__[op] = suberrs
             elif op == "oneOf" and num_fails != num_rules - 1:
-                err.__dict__[op] = val
+                err.__dict__[op] = suberrs
             elif op == "anyOf" and num_fails == num_rules:
-                err.__dict__[op] = val
+                err.__dict__[op] = suberrs
             # print("num rules:", num_rules, "num fails: ", num_fails)
-
-        # finally, perform implication / "then" constraint
-        # on failure, set the rewritten path into the error (if different to normal path)
-        if not err and rl.then:
-            # print(f"for path {path} eval then: ", rl.then)
-            err.then = self.validate_path(thenPath, rl.then, curCtx)
-            if err.then and thenPath != path:
-                err._rewritePath = thenPath
-
-        if ctx.debug:
-            print(f"return validate_rule for '{path}': {repr(err)}")
 
         if err:
             return DSRule(__root__=err)
-        return None
+
+        # 4. perform implication / "then" constraint
+        # on failure, set the rewritten path into the error (if different to normal path)
+        if rl.then:
+            thenErr = self.validate_path(thenPath, rl.then, curCtx)
+            if thenErr is not None:
+                err = cast(Rule, thenErr.__root__)  # technically a lie, but morally ok
+                if not isinstance(err, bool) and thenPath != path:
+                    err._rewritePath = thenPath
+
+        # if ctx.debug:
+        #     print(f"return validate_rule for '{path}': {repr(err)}")
+
+        if err:
+            return DSRule(__root__=err)
+        return None  # no errors
